ffadda45cca8f332a6cbb4e8f6b456fd78583e58
Abedolla2302 <abedollaalcantar@csumb.edu>
2024-09-06 16:13:56 -0700

    Created route from login to homepage.js. Added test files for signup, login, homepage.

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Import the screens
+import Login from './screens/login';
+import HomePage from './screens/homepage';
+
+// Create a Stack Navigator
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcoming to a working App Group 5 :D</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="homepage" component={HomePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
